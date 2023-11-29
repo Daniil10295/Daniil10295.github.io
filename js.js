9d33441ad602b52587135e1eb0a095f74afabd4d
Daniil10295 <151400837+Daniil10295@users.noreply.github.com>
2023-11-29 20:13:42 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,6 +2,6 @@ var siteBlock = document.getElementById('site');
 var waitBlock = document.getElementById('wait');
 
 setTimeout(function() {
- siteBlock.classList.remove('hidden');
- waitBlock.classList.add('hidden')
+ siteBlock.removeAttribute('hidden');
+ waitBlock.setAttribute('hidden', '')
 }, 2000);
